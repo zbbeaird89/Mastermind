342bca0c170609aa67d056d57296cb2f9b2ef542
Zach Beaird <zbbeaird1989@gmail.com>
2016-12-12 19:57:04 -0600

    Creates and tests get_hole and set_hole

--- lib/mastermind/board.rb
+++ lib/mastermind/board.rb
@@ -1,8 +1,17 @@
 module Mastermind
 	class Board 
 		attr_reader :row
+
 		def initialize(input = {})
 			@row = input.fetch(:row, default_row)
+		end
+
+		def get_hole(hole) 
+			@row[hole - 1]
+		end
+
+		def set_hole(hole, value)
+			get_hole(hole).value = value
 		end
 
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,12 +12,21 @@ module Mastermind
 			end
 		end
 
-		context "#default_row" do 
-			it "calls default_row by default" do 
-				board = Board.new
-				board.row.each do |hole|
-					expect(hole.value).to eq ""
-				end
+		context "#get_hole" do 
+			it "returns the hole specified" do
+				example = ["R", "B", "G", "Y"] 
+				board = Board.new({row: example})
+				expect(board.get_hole(1)).to eq "R"
+			end
+		end
+
+		context "#set_hole" do 
+			it "updates the specified hole with a new value" do 
+				Dog = Struct.new(:value)
+				example = [Dog.new("R"), "B", "G", "Y"]
+				board = Board.new({row: example})
+				board.set_hole(1, "O")
+				expect(board.get_hole(1).value).to eq "O"
 			end
 		end
 
@@ -27,5 +36,15 @@ module Mastermind
 				expect(board.row).to eq "hello"
 			end
 		end
+
+		context "#default_row" do 
+			it "calls default_row by default" do 
+				board = Board.new
+				board.row.each do |hole|
+					expect(hole.value).to eq ""
+				end
+			end
+		end
+
 	end
 end
